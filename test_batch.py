6ae27c90446689d7e88f05e8b7803b92efa3f2d4
pavanabharath24 <korukondapavanbharath@gmail.com>
2026-08-19 03:21:55 +0530

    Clean up junk files and pycache from PR

--- test_batch.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-from backend.models.predictor import predictor
-predictor.load()
-
-df = pd.read_csv("data/patient_churn_dataset_enriched.csv")
-res = predictor.predict_batch(df, df.copy())
-print(json.dumps(res[0]))
